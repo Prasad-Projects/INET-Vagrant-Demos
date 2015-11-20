a8938553b7eba35b4b8388eb5e699a47a3a537b1
systemslab <systemslab@systemslab-OptiPlex-7010.(none)>
2015-11-20 13:40:31 +0530

    scripts in DTN_Demo updated

--- DTN_Demo/DTN_only.bash
+++ DTN_Demo/DTN_only.bash
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.deb
-dpkg -i vagrant_1.7.4_x86_64.deb
-git clone https://github.com/prasadtalasila/INET-Vagrant-Demos.git
-vagrant box add hashicorp/precise64
-cd INET-Vagrant-Demos/DTN\ Demo1/Machine1
+#../Installation_Script.bash
+cd Machine1
 vagrant up
 cd ../Machine2
 vagrant up
